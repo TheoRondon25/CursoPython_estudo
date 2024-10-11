33afbebf253588ba3891da18e7a1fff261224392
Theo Rondon <theoprondon@hotmail.com>
2024-10-11 19:13:56 -0300

    estudo 11/10

---
+++ aula169.py
@@ -0,0 +1,13 @@
+# Formatando datas do datetime
+# datetime.strftime('DATA', 'FORMATO')
+# https://docs.python.org/3/library/datetime.html
+from datetime import datetime
+
+fmt = '%d/%m/%Y'
+# data = datetime(2022, 12, 13, 7, 59, 23)
+data = datetime.strptime('2022-12-13 07:59:23', '%Y-%m-%d %H:%M:%S')
+print(data.strftime(fmt))
+print(data.strftime('%d/%m/%Y %H:%M'))
+print(data.strftime('%Y'), data.year)
+print(data.strftime('%m'), data.month)
+print(data.strftime('%d'), data.day)
